beb808517286ede0bd435483fb98854af74e44c1
jeons <etoile929@naver.com>
2024-08-14 23:50:26 +0900

    test

--- index.php
+++ index.php
@@ -65,6 +65,50 @@
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 	<!-- Mustache CDN -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.1/mustache.min.js"></script>
+	<script>
+		function openChat() {
+			$(document).ready(function(){
+				loadMemberList();
+			});
+
+			function loadMemberList() {
+					//1. 데이터베이스의 회원 정보를 읽어 json 객체 형태로 받는 것
+					$.ajax({
+					  type: 'POST',  
+					  url: "getMemberList.php",
+					  data: {},
+					  dataType : 'text',
+					  cache: false,
+					  async: false
+					})
+					.done(function( result ) {
+					  //성공했을때
+					  $("#divChatOrMember").load("member.php", function(){
+					    MEMBER_ROOM_MODE = "MEMBER";
+						let memberList = {"MEMBER": JSON.parse(result)};
+					
+						//2. 받은 내용을 무스타크로 출력하는 것
+						var output = Mustache.render($("#divMemberList").html(), memberList);	
+						$("#divMemberList").html(output);	
+					
+						$('.class_icon').css('color', '#909297'); 
+						$("#icon_member").css('color', 'black');
+					  });
+					})
+					.fail(function( result, status, error ) {
+							//실패했을때
+							alert("에러 발생:" + error);
+					});
+			}		
+
+			$("#MAIN").css("left",($(document).width() + 100)); //MAIN 창을 안보이게 해줌 
+			$("#MAIN").load("chat.php", function(){
+				$("#MAIN").animate({left:0, top:0});
+			});//다시 MAIN창의 재설정 
+
+
+		}
+	</script>
 	<script src="/jquery.bpopup.min.js"></script>
 	<script>
 		let websocket = null; //웹소켓을 통해 서버와 연결
@@ -198,36 +242,6 @@
 			websocket.send(JSON.stringify(msg));
 		}
 
-		function loadMemberList() {
-			//1. 데이터베이스의 회원 정보를 읽어 json 객체 형태로 받는 것
-			$.ajax({
-			  type: 'POST',  
-			  url: "getMemberList.php",
-			  data: {},
-			  dataType : 'text',
-			  cache: false,
-			  async: false
-			})
-			.done(function( result ) {
-			  //성공했을때
-			  $("#divChatOrMember").load("member.php", function(){
-			    MEMBER_ROOM_MODE = "MEMBER";
-				let memberList = {"MEMBER": JSON.parse(result)};
-
-				//2. 받은 내용을 무스타크로 출력하는 것
-				var output = Mustache.render($("#divMemberList").html(), memberList);	
-				$("#divMemberList").html(output);	
-
-				$('.class_icon').css('color', '#909297'); 
-				$("#icon_member").css('color', 'black');
-			  });
-			})
-			.fail(function( result, status, error ) {
-					//실패했을때
-					alert("에러 발생:" + error);
-			});
-		}
-
 		function loadChatMemberList() {
 			$("#MAIN").css("left", (0 - $(document).width()));
 			$("#MAIN").load("chat_member_container.php", function(){
@@ -241,7 +255,8 @@
 			});
 		}
 
-		function openChat(you_member_code, you_member_alias) {
+		//친구 목록을 클릭 -> 대화방 이동 
+        function openChat(you_member_code, you_member_alias) {
 
 			let members = [];
 			let me = {"memberCode": "<?php echo $_SESSION["kakao_member_code"]?>","memberAlias": "<?php echo $_SESSION["kakao_member_alias"]?>"}
